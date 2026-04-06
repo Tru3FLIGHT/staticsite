746f94cdcd0e08df113a7f3c2737cf2123e3a441
zalea <zackarymschaefer@gmail.com>
2026-04-06 11:29:49 -0500

    will now generate based on the whole content directory

--- src/main.py
+++ src/main.py
@@ -4,14 +4,25 @@ import shutil
 from pagegen import generate_page
 from sitegen import copy_static
 
-STATIC_PATH = "/home/zalea/Documents/Projects/bootdev/staticsite/static/"
-PUBLIC_PATH = "/home/zalea/Documents/Projects/bootdev/staticsite/public/"
+STATIC_PATH = "static/"
+PUBLIC_PATH = "public/"
+CONTENT_PATH = "content/"
+TEMPLATE = "template.html"
 
 def main() -> None:
     print("Running Static site Generator")
     copy_static(STATIC_PATH, PUBLIC_PATH)
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    gen_content(CONTENT_PATH, PUBLIC_PATH)
+
+def gen_content(content_dir:str, destination:str):
+    for file in os.listdir(content_dir):
+        fullpath = content_dir + file
+        print(fullpath)
+        if os.path.isdir(fullpath):
+            gen_content(content_dir+file+"/", destination+file+"/")
+        if file == "index.md":
+            generate_page(fullpath, TEMPLATE, destination+"index.html")
 
 if __name__ == "__main__":
     main()
