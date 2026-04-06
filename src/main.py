c7756deb19d5165646bd968dc46dc255a0ecff82
zalea <zackarymschaefer@gmail.com>
2026-04-06 09:53:42 -0500

    added copy_static() function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 import os
 import shutil
 
+from sitegen import copy_static
+
+STATIC_PATH = "/home/zalea/Documents/Projects/bootdev/staticsite/static/"
+PUBLIC_PATH = "/home/zalea/Documents/Projects/bootdev/staticsite/public/"
+
 def main() -> None:
     print("Running Static site Generator")
+    for path in os.listdir(STATIC_PATH):
+        print(path)
+    copy_static(STATIC_PATH, PUBLIC_PATH)
 
 if __name__ == "__main__":
     main()
--- src/sitegen.py
+++ src/sitegen.py
@@ -1,5 +1,24 @@
 import os
 import shutil
 
-def copy_static():
-    pass
+def copy_static(source: str, destination: str, dest_clear:bool=False):
+    if not dest_clear and os.path.exists(destination):
+        shutil.rmtree(destination)
+        print(f"cleaing {destination}")
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    dest_clear = True
+
+    if not os.path.exists(source):
+        raise OSError(f"Cannot Access {source}, no such file or directory")
+    print("copying files")
+    for file in os.listdir(source):
+        filepath = source + file
+        print(f"File: {filepath}")
+        if os.path.isfile(filepath):
+            shutil.copy(filepath, destination)
+            print(f"copying file {filepath}, to {destination}")
+        if os.path.isdir(filepath):
+            print(f"entering {filepath} directory")
+            copy_static(filepath + "/", destination+file+"/", True)
+            print(f"exiting {filepath} directory")
